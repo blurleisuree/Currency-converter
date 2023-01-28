321420d286cbf4a2bb9d28b20dd774ddaf92e26f
blurleisuree <ipishir@gmail.com>
2023-01-28 20:14:02 +0300

    add swap btn functional

--- js/index.js
+++ js/index.js
@@ -1,5 +1,38 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    // Swap --------------------------------------------------------------------------
+
+    const swapBtn = $(".swap-btn");
+
+    swapBtn.css("transition", "all 0.25s ease-out");
+
+    let click = false, // Проверка был ли клик
+        rotate = 0;
+
+    swapBtn.hover(function () {
+        rotate += 90
+        $(this).css("transform", `rotate(${rotate}deg)`);
+    }, function () {
+        if (click == true) {
+            click = false // Обнуление
+            return
+        }
+        rotate -= 90
+        $(this).css("transform", `rotate(${rotate}deg)`);
+    })
+
+    let swapClick = () => {
+
+        // тут должен быть скрипт для сваппа
+
+        rotate += 90;
+        $(swapBtn).css("transform", `rotate(${rotate}deg)`);
+        click = true;
+    }
+
+    swapBtn.click(swapClick)
+
+    // 
 
     
-
 });
